f07d855227f9df6531bf74c992edc48c6abf65bf
Eugene Sharygin <eush77@gmail.com>
2015-02-15 16:58:03 +0300

    Add new directories to the top of the list

--- src/install.fish
+++ src/install.fish
@@ -1,7 +1,7 @@
 set source_dir $argv[1]
 
 if not contains $source_dir $fish_function_path
-  set --universal fish_function_path $fish_function_path $source_dir
+  set --universal fish_function_path $source_dir $fish_function_path
   printf "+ %s\n" $source_dir
 else
   printf "- %s (already installed)\n" $source_dir
