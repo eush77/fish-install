4b8dc31245856c7015d3a598d6e65349b62e30e0
Eugene Sharygin <eush77@gmail.com>
2015-02-25 16:12:50 +0300

    Detect if fish session is corrupt (fix #2)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+require('object.assign').shim();
 
 var once = require('once');
 
@@ -12,8 +13,14 @@ module.exports = function (sourceDir, cb) {
   sourceDir = path.resolve(sourceDir);
   cb = once(cb);
 
-  spawn('fish', [install, sourceDir], { stdio: 'inherit' })
-    .on('error', cb)
+  var env = Object.assign({}, process.env, {
+    'FISH_INSTALL_PATH': sourceDir
+  });
+
+  spawn('fish', [install, sourceDir], {
+    stdio: 'inherit',
+    env: env
+  }).on('error', cb)
     .on('exit', function (code, signal) {
       var error = (code != 0)
                 ? new Error('Fish did not terminate normally')
--- src/install.fish
+++ src/install.fish
@@ -1,5 +1,17 @@
 set source_dir $argv[1]
 
+
+set fish_error (fish -c true ^&1 >/dev/null)
+if test -n "$fish_error"
+  set_color red
+  printf "\nFish is unable to start correctly due to downgraded privileges.\n"
+  printf "Please install fish-install globally and run:\n"
+  printf "  > fish-install $FISH_INSTALL_PATH\n"
+  set_color normal
+  exit 0
+end
+
+
 if not contains $source_dir $fish_function_path
   set --universal fish_function_path $source_dir $fish_function_path
   printf "+ %s\n" $source_dir
