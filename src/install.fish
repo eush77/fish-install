6c4d868d2f0c17c5c93d896ebd1cb52ff33afda3
Eugene Sharygin <eush77@gmail.com>
2015-02-27 02:26:32 +0300

    Change indication of already installed dirs

--- src/install.fish
+++ src/install.fish
@@ -16,5 +16,5 @@ if not contains $source_dir $fish_function_path
   set --universal fish_function_path $source_dir $fish_function_path
   printf "+ %s\n" $source_dir
 else
-  printf "- %s (already installed)\n" $source_dir
+  printf "= %s (already installed)\n" $source_dir
 end
